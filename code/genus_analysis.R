3a38d81d8327fedcbe3a3230b105caa8bcde4c08
Stefano Scansani <75729718+sscansan@users.noreply.github.com>
2022-04-17 15:43:35 +0200

    roc curves of the significant genera

--- code/genus_analysis.R
+++ code/genus_analysis.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(broom)
 library(ggtext)
 
+set.seed(14091989)
+
 # feature tab;e
 shared <- 
   read_tsv("raw_data/baxter.subsample.shared",
@@ -98,11 +100,11 @@ composite %>%
   scale_x_log10() +
   scale_color_manual(NULL, 
                      breaks = c(F, T),
-                     values = c("gray", "dodgerblue"),
+                     values = c("gray", "steelblue"),
                      labels = c("Healthy", "SRN")) +
   scale_fill_manual(NULL, 
                      breaks = c(F, T),
-                     values = c("gray", "dodgerblue"),
+                     values = c("gray", "steelblue"),
                      labels = c("Healthy", "SRN")) +
   labs(x = "Relative abundance (%)",
        y = NULL) +
@@ -111,3 +113,79 @@ composite %>%
 
 ggsave("figures/significant_genera.png", width = 5, height = 4)
 
+
+
+
+test <-
+  composite %>% 
+  inner_join(sig_genera, by = "taxonomy") %>% 
+  select(group, taxonomy, rel_abund, fit_result, srn) %>% 
+  pivot_wider(names_from = taxonomy, values_from = rel_abund) %>% 
+  pivot_longer(cols = -c(group, srn), names_to =  "metric", values_to = "score") %>% 
+  filter(metric == "fit_result")
+
+
+get_sens_spec <-
+  function(treshold, score, actual, direction) {
+    
+    # treshold <- 100
+    # score <- test$score
+    # actual <- test$srn
+    # direction <- "greaterthan"
+    
+    predicted <- if(direction == "greaterthan") {score > treshold} else{score < treshold}
+    
+    tp <- sum(predicted & actual)
+    tn <- sum(!predicted & !actual)
+    fp <- sum(predicted & !actual)
+    fn <- sum(!predicted & actual)
+    
+    specificity <- tn / (tn + fp)
+    sensitivity <- tp / (tp + fn)
+    
+    tibble("specificity" = specificity,
+           "sensitivity" = sensitivity)
+  }
+    
+
+
+get_roc_data <- 
+  function(x, direction) {
+  
+  # x <- test
+  # direction <- "greaterthan"
+  
+  tresholds = unique(x$score) %>%  sort()
+  
+  map_dfr(.x = tresholds, ~get_sens_spec(.x, x$score, x$srn, direction)) %>% 
+    rbind(c(specificity = 0, sensitivity = 1))
+  
+}
+
+
+# get_sens_spec(100, test$score, test$srn, "greaterthan")
+# get_roc_data(test, "greaterthan")
+
+
+roc_data <-
+  composite %>% 
+  inner_join(sig_genera, by = "taxonomy") %>% 
+  select(group, taxonomy, rel_abund, fit_result, srn) %>% 
+  pivot_wider(names_from = taxonomy, values_from = rel_abund) %>% 
+  pivot_longer(cols = -c(group, srn), names_to =  "metric", values_to = "score") %>% 
+  # filter(metric == "fit_result") %>% 
+  nest(data = -metric) %>% 
+  mutate(direction = ifelse(metric == "Lachnospiraceae_unclassified",
+                            "lessthan", "greaterthan")) %>% 
+  mutate(roc_data = map2(.x = data, .y = direction, ~get_roc_data(.x, .y))) %>% 
+  unnest(roc_data) %>% 
+  select(metric, specificity, sensitivity)
+
+roc_data %>% 
+  ggplot(aes(x = 1-specificity, y= sensitivity, color = metric)) +
+  geom_line() +
+  geom_abline(slope = 1, intercept = 0, color = "gray75", linetype = "dashed") +
+  theme_classic() +
+  coord_fixed()
+
+ggsave("figures/roc_figure.png", height = 4, width = 6)
